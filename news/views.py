6e2cc154a1faeac8926840b0a6020a022c7a5289
Rohini Mishra <iit2018087@iiita.ac.in>
2020-09-07 03:19:42 +0530

    added navbar

--- news/views.py
+++ news/views.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
 toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
 toi_headings = toi_soup.find_all('h2')
-toi_headings = toi_headings[0:25]
+toi_headings = toi_headings[3:20]
 toi_news = []
 
 for th in toi_headings:
@@ -17,7 +17,7 @@ for th in toi_headings:
 ht_r = requests.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen")
 ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
 ht_headings = ht_soup.findAll('h3')
-ht_headings = ht_headings[0:25]
+ht_headings = ht_headings[3:20]
 ht_news = []
 
 
