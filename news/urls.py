7ecd29ce5333facdb09fefbced9cccd02c8f8e5c
ananya-mohanty <42179851+ananya-mohanty@users.noreply.github.com>
2020-09-14 22:33:35 +0530

    Authentication using Firebase

--- news/urls.py
+++ news/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('readAloud/', views.readAloud, name='readAloud'),
     path('stop/', views.stop, name='stop'),
+    path('signin/', views.signIn),
+    path('postsign/', views.postsign),
+    path('logout/', views.logout, name="log"),
+    path('signup/', views.signUp, name="signup"),
+    path('postsignup/', views.postsignup, name="postsignup")
 ]
