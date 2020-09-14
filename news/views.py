a37f1a499f8be30ed94d5448659743c60b1e0502
griffy06 <hi2cheena@gmail.com>
2020-09-14 18:52:57 +0530

    topic segregation from times of india

--- news/views.py
+++ news/views.py
@@ -4,34 +4,54 @@ from bs4 import BeautifulSoup
 import pyttsx3
 from . import apps
 
-headlines=[]
-# Getting news from Times of India
-toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
-toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
-toi_headings = toi_soup.find_all('h2')
-toi_headings = toi_headings[3:20]
-toi_news = []
-
-headlines.append('News from Times of India are as follows:')
-for th in toi_headings:
-    headlines.append(th.text)
-    toi_news.append(th.text)
-
-# Getting news from Hindustan Times
-ht_r = requests.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen")
-ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
-ht_headings = ht_soup.findAll('h3')
-ht_headings = ht_headings[3:20]
-ht_news = []
-
-headlines.append('News from Hindustan Times are as follows:')
-for hth in ht_headings:
-    headlines.append(hth.text)
-    ht_news.append(hth.text)
-
 
 def index(req):
-    return render(req, 'news/index.html', {'toi_news':toi_news, 'ht_news': ht_news})
+    urlVar="https://timesofindia.indiatimes.com/briefs"
+    return display(req,urlVar)
+
+
+def index2(req):
+    urlVar = "https://timesofindia.indiatimes.com/briefs/entertainment"
+    return display(req, urlVar)
+
+
+def index3(req):
+    urlVar = "https://timesofindia.indiatimes.com/briefs/business"
+    return display(req, urlVar)
+
+
+def index4(req):
+    urlVar = "https://timesofindia.indiatimes.com/briefs/sports"
+    return display(req, urlVar)
+
+
+def display(req,urlVar):
+    headlines = []
+    toi_r = requests.get(urlVar)
+    toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
+    toi_headings = toi_soup.find_all('h2')
+    toi_headings = toi_headings[3:20]
+    toi_news = []
+
+    headlines.append('News from Times of India are as follows:')
+    for th in toi_headings:
+        headlines.append(th.text)
+        toi_news.append(th.text)
+
+    # Getting news from Hindustan Times
+    ht_r = requests.get("https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen")
+    ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
+    ht_headings = ht_soup.findAll('h3')
+    ht_headings = ht_headings[3:20]
+    ht_news = []
+
+    headlines.append('News from Hindustan Times are as follows:')
+    for hth in ht_headings:
+        headlines.append(hth.text)
+        ht_news.append(hth.text)
+
+    return render(req, 'news/index.html', {'toi_news': toi_news, 'ht_news': ht_news})
+
 
 def readAloud(req):
     engine = pyttsx3.init()
