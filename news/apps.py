84066e81926c31b148671e256d8fa1071871fc97
Rohini Mishra <iit2018087@iiita.ac.in>
2020-09-25 23:55:52 +0530

    topics feature completed

--- news/apps.py
+++ news/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
 
-flag=False
-idx=0
-headlines=[]
-toi_news=[]
-ht_news=[]
+flag = False
+idx = 0
+headlines = []
+toi_news = []
+ht_news = []
+
 
 class NewsConfig(AppConfig):
     name = 'news'
